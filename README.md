8724a9fa91936f6c058da8fecbc7f2632a5be3fa
Anselmos <anselmos@users.noreply.github.com>
2017-04-05 06:44:11 +0200

    Adds tarit script

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ This repository contains scripts to make my life easier.
 # Scripts available:
 
 - archive: Makes a tar.gz file from input parameter and *REMOVES* origin source.
+- tarit: Makes a tar.gz file *WITHOUT REMOVING* origin source.
 - dearchive: Makes origin source from tar.gz - NOTE : *DOES NOT REMOVE* .tar.gz file!
--- install.sh
+++ install.sh
@@ -5,9 +5,11 @@ install_path=$bin_path/linux-utils
 
 mkdir $install_path
 cp archive $install_path/
+cp tarit $install_path/
 cp dearchive $install_path/
 cp acquire $install_path/
 
 ln -s $install_path/archive $bin_path/archive
+ln -s $install_path/tarit $bin_path/tarit
 ln -s $install_path/dearchive $bin_path/dearchive
 ln -s $install_path/acquire $bin_path/acquire
