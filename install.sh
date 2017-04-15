b2946d9e96f51ae8a317b0f69953843401012dcc
anselmos <anselmos@users.noreply.github.com>
2017-04-15 17:14:34 +0200

    Adds markdown_commits in it's pre-early stage to linux-utils

--- install.sh
+++ install.sh
@@ -8,8 +8,10 @@ cp archive $install_path/
 cp tarit $install_path/
 cp dearchive $install_path/
 cp acquire $install_path/
+cp markdown_commits $install_path/
 
 ln -s $install_path/archive $bin_path/archive
 ln -s $install_path/tarit $bin_path/tarit
 ln -s $install_path/dearchive $bin_path/dearchive
 ln -s $install_path/acquire $bin_path/acquire
+ln -s $install_path/markdown_commits $bin_path/markdown_commits
