4d83e3c41161b4c1f190fac4a3b7206e1efb3f70
Anselmos <anselmos@users.noreply.github.com>
2018-03-06 15:58:27 +0100

    Adds Fixing ubuntu1404 clock and installing deadbeef

---
+++ fix_ubuntu1404_clock.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+#
+# fix_ubuntu1404_clock.sh
+# Copyright (C) 2018 [Anselmos](github.com/anselmos) <anselmos@users.noreply.github.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+pkill -f indicator-datetime
---
+++ install_deadbeef.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo add-apt-repository ppa:starws-box/deadbeef-player
+sudo apt-get update
+sudo apt-get install deadbeef
